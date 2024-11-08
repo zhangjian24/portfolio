9b67b37ad90d847a7ea3159b5fcc0c30020ec3aa
zhangjian24 <2167862626@qq.com>
2024-11-08 23:40:07 +0800

    title

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE = 'https://portfolio.jianzhang.site';
-export const SITE_TITLE = 'Portfolio';
-export const SITE_DESCRIPTION = 'Portfolio';
+export const SITE_TITLE = '健兼';
+export const SITE_DESCRIPTION = '健兼';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
