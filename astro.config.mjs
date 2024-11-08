3f3fd895e7d8938103db76c8870c38de8a866bb3
zhangjian24 <2167862626@qq.com>
2024-11-08 22:36:22 +0800

    wechat,code block

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+import expressiveCode from 'astro-expressive-code';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://portfolio.jianzhang.site',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [expressiveCode(), mdx(), sitemap(), tailwind(), ]
 });
