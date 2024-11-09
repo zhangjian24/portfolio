e2b9483040051af5a8156565887f404f1fd181c9
zhangjian24 <2167862626@qq.com>
2024-11-09 11:14:14 +0800

    table of content

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,21 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import rehypeToc from '@jsdevtools/rehype-toc'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 
 import expressiveCode from 'astro-expressive-code';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://portfolio.jianzhang.site',
-  integrations: [expressiveCode(), mdx(), sitemap(), tailwind(), ]
+  integrations: [expressiveCode(), mdx(), sitemap(), tailwind(), ],
+  markdown: {
+		rehypePlugins: [
+      rehypeSlug,
+      [rehypeAutolinkHeadings, { behavior: 'prepend' }],
+			[rehypeToc, { headings: ['h1','h2', 'h3'] }],
+		],
+	},
 });
